8f2f7a726798a9f7c71969598660419d73551b6d
DorTaub <dort1000@gmail.com>
2022-04-24 21:07:38 +0300

    My Test

--- Web/main.js
+++ Web/main.js
@@ -5,6 +5,7 @@ function calculate() {
 	const posY = document.querySelector('#yVal');
 	const myCanvas = document.querySelector('#myCanvas');
 	const painter = myCanvas.getContext('2d');
+	const spanText = document.querySelector('#spanText');
 
 	if (
 		width.value === '' ||
@@ -20,14 +21,28 @@ function calculate() {
 	painter.strokeRect(width.value, height.value, posX.value, posY.value);
 	painter.fillStyle = 'blue';
 	painter.fillRect(width.value, height.value, posX.value, posY.value);
-	console.log(width.value);
+	spanText.innerText = width.value * height.value;
+	spanText.style.display = 'inline-block';
+}
+function clear() {
+	const width = document.querySelector('#widVal');
+	const height = document.querySelector('#heiVal');
+	const posX = document.querySelector('#xVal');
+	const posY = document.querySelector('#yVal');
+	const myCanvas = document.querySelector('#myCanvas');
+	const painter = myCanvas.getContext('2d');
+	painter.clearRect(0, 0, 300, 300);
+	width.value = '';
+	height.value = '';
+	posX.value = '';
+	posY.value = '';
 }
 
 function onWindowLoad() {
 	const calBtn = document.querySelector('#calBtn');
 	calBtn.addEventListener('click', calculate);
 	const resBtn = document.querySelector('#resBtn');
-	resBtn.addEventListener('click', calculate);
+	resBtn.addEventListener('click', clear);
 }
 
 window.onload = onWindowLoad;
